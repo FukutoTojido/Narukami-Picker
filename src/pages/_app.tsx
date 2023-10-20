4237136136aab144c6de177b186bd9dfdc73f656
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2023-10-20 13:46:18 +0700

    Remove picker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { MuseoModerno } from "next/font/google";
 
 const mm = MuseoModerno({
-    weight: ["400", "500", "600", "700", "800", "900"],
     subsets: ["latin"],
 });
 
